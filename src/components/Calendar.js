961d7712554bfe001a3085e53360143b99c19db7
Tagacae <85138779+Tagaca19e@users.noreply.github.com>
2021-09-27 22:32:24 -0700

    Update Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import BigCalendar from "react-big-calendar";
 import moment from "moment";
 import Dialog from "material-ui/Dialog";
-import FlatButton from "material-ui/FlatButton";
+
 import TextField from "material-ui/TextField";
 import TimePicker from "material-ui/TimePicker";
 require("react-big-calendar/lib/css/react-big-calendar.css");
